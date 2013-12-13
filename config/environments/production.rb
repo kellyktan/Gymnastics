0ab5b384189fad1a03c81562893aa07d293d5554
Kelly Tan <kellytan@seas.upenn.edu>
2013-12-13 18:02:06 -0500

    Fixed email link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ MeetDatabase::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => "sheltered-hollows-7106.herokuapp.com"}
+  config.action_mailer.default_url_options = {:host => "my-gymnastics-database.herokuapp.com"}
   
   config.action_mailer.smtp_settings = {
       :address => 'smtp.sendgrid.net',
